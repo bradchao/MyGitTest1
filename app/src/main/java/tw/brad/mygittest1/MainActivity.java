c0c7bae9ef5b0f2f1280fdd55fcbaae7e03f10b5
Brad Chao <brad@brad.tw>
2017-09-28 15:23:57 +0800

    v4

--- app/src/main/java/tw/brad/mygittest1/MainActivity.java
+++ app/src/main/java/tw/brad/mygittest1/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("Brad", "OK");
+        Log.i("Brad", "OK2");
 
     }
 }
